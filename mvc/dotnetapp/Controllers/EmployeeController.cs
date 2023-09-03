36f4f549ed40a3efaffefbd9127124307b673f7d
Iamneo <techadmin@iamneo.ai>
2023-09-03 18:39:14 +0000

    setup commit

--- mvc/dotnetapp/Controllers/EmployeeController.cs
+++ mvc/dotnetapp/Controllers/EmployeeController.cs
@@ -11,7 +11,7 @@ using Microsoft.Data.SqlClient;
 
 public class EmployeeController : Controller
 {
-    private string connectionString = "User ID=sa;password=examlyMssql@123;server=dfebcbcfaafeabdbcfacbdcbaeadbebabcdebdca-0;Database=EmployeeDB;trusted_connection=false;Persist Security Info=False;Encrypt=False";
+    private string connectionString = "User ID=sa;password=examlyMssql@123;server=dffafdafebcfacbdcbaeadbebabcdebdca-0;Database=EmployeeDB;trusted_connection=false;Persist Security Info=False;Encrypt=False";
 
     public ActionResult Index()
     {
@@ -95,115 +95,51 @@ catch(Exception ex)
     }
 
 
-    public ActionResult Edit(int id)
+
+    public ActionResult Search(string query)
+{
+    try
     {
-        try{
-    Employee employee = null;
-    using (SqlConnection connection = new SqlConnection(connectionString))
-    {
-        string query = "SELECT * FROM Employee WHERE id = @id";
-        using (SqlCommand command = new SqlCommand(query, connection))
+        List<Employee> employees = new List<Employee>();
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
         {
-            command.Parameters.AddWithValue("@id", id);
-            connection.Open();
-            SqlDataReader reader = command.ExecuteReader();
-            while (reader.Read())
+            string searchQuery = "SELECT * FROM Employee WHERE id LIKE @query OR Name LIKE @query";
+            using (SqlCommand command = new SqlCommand(searchQuery, connection))
             {
-                employee = new Employee();
+                command.Parameters.AddWithValue("@query", "%" + query + "%"); // Wildcard search
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Employee employee = new Employee();
                     employee.id = Convert.ToInt32(reader["id"]);
                     employee.Name = reader["Name"].ToString();
                     employee.DOB = DateTime.Parse(reader["DOB"].ToString());
                     employee.Gender = reader["Gender"].ToString();
                     employee.Department = reader["Department"].ToString();
                     employee.Position = reader["Position"].ToString();
-                    employee.salary = Convert.ToDecimal(reader["salary"]);;
+                    employee.salary = Convert.ToDecimal(reader["Salary"]);
                     employee.Email = reader["Email"].ToString();
                     employee.phoneNumber = reader["phoneNumber"].ToString();
-                    employee.Address = reader["Address"].ToString();                   
-            }
+                    employee.Address = reader["Address"].ToString();
+                    employees.Add(employee);
+                }
 
-            reader.Close();
+                reader.Close();
+            }
         }
+
+        return View("Index", employees);
     }
-    if (employee == null)
-        {
-            return NotFound();
-        }
-    return View(employee);
-        }catch (Exception ex)
+    catch (Exception ex)
     {
         Console.WriteLine(ex.Message);
-        // You can handle the exception as per your requirements (e.g., logging, displaying an error message)
-        return BadRequest("An error occurred while retrieving the employee item.");
+        return BadRequest("An error occurred while searching for employees.");
     }
-
 }
-    [HttpPost]
-    public ActionResult Edit(Employee employee )
-    {
-        try{
-        using (SqlConnection connection = new SqlConnection(connectionString))
-        {
-            string query = "UPDATE Employee SET Name = @Name, DOB = @DOB, Gender = @Gender, Department = @Department, Position = @Position, Salary = @Salary, Email = @Email, phoneNumber = @phoneNumber, Address = @Address WHERE id = @id";
 
-            using (SqlCommand command = new SqlCommand(query, connection))
-            {
-                command.Parameters.AddWithValue("@id", employee.id);
-                command.Parameters.AddWithValue("@Name", employee.Name);
-                command.Parameters.AddWithValue("@DOB", employee.DOB);
-                command.Parameters.AddWithValue("@Gender", employee.Gender);
-                command.Parameters.AddWithValue("@Department", employee.Department);
-                command.Parameters.AddWithValue("@Position", employee.Position);
-                command.Parameters.AddWithValue("@salary", employee.salary);
-                command.Parameters.AddWithValue("@Email", employee.Email);
-                command.Parameters.AddWithValue("@phoneNumber", employee.phoneNumber);
-                command.Parameters.AddWithValue("@Address", employee.Address);
-                connection.Open();
-                int rowsAffected = command.ExecuteNonQuery();
 
-                if (rowsAffected == 0)
-                {
-                    // The provided ID does not exist in the Furniture table
-                    return NotFound();
-                }
-            }
-        }
-        }catch(Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
-        return RedirectToAction("Index");
-        
-    }
-    public ActionResult Delete(int id)
-    {
-        try{
-            if (id <= 0)
-        {
-            return BadRequest();
-        }
-    using (SqlConnection connection = new SqlConnection(connectionString))
-    {
-        string query = "DELETE FROM Employee WHERE id = @id";
-
-        using (SqlCommand command = new SqlCommand(query, connection))
-        {
-            command.Parameters.AddWithValue("@id", id);
-            connection.Open();
-            int rowsAffected = command.ExecuteNonQuery();
-
-                if (rowsAffected == 0)
-                {
-                    return NotFound();
-                }
-        }
-    }}
-    catch(Exception ex)
-{
-    Console.WriteLine(ex.Message);
-    return BadRequest("An error occurred while deleting the employee item.");
-
-}
-    return RedirectToAction("Index");
-    }
 }
--- mvc/dotnetapp/TestProject/UnitTest1.cs
+++ mvc/dotnetapp/TestProject/UnitTest1.cs
@@ -2,85 +2,216 @@ using NUnit.Framework;
 using dotnetapp.Controllers;
 using dotnetapp.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
+using System;
+using System.Reflection;
+using Moq;
+using System.Data;
+using System.Data.SqlClient;
 
-namespace EmployeeControllerTests
+namespace EmployeeApp.Tests
 {
     [TestFixture]
     public class EmployeeControllerTests
     {
-        private EmployeeController _controller;
+        private Type controllerType;
+        private Type employeeType;
+        private PropertyInfo[] properties;
 
         [SetUp]
         public void Setup()
         {
-            // Initialize the controller before each test
-            _controller = new EmployeeController();
+            // Get the type of the EmployeeController
+             employeeType = typeof(dotnetapp.Models.Employee);
+            properties = employeeType.GetProperties();
+            controllerType = typeof(EmployeeController);
         }
 
         [Test]
-        public void test_case1()
-        {
-            // Act
-            var result = _controller.Index() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.IsInstanceOf<ViewResult>(result);
-            Assert.IsAssignableFrom<List<Employee>>(result.Model);
-        }
-
-        [Test]
-        public void test_case2()
-        {
-            // Act
-            var result = _controller.Create() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.IsInstanceOf<ViewResult>(result);
-        }
-
-        [Test]
-        public void test_case3()
+        public void TestIndexMethodExists()
         {
             // Arrange
-            int nonExistingId = 100; // Assuming there is no employee with ID 100
-
-            // Act
-            var result = _controller.Edit(nonExistingId) as NotFoundResult;
+            MethodInfo indexMethod = controllerType.GetMethod("Index");
 
             // Assert
-            Assert.IsNotNull(result);
-            Assert.IsInstanceOf<NotFoundResult>(result);
+            Assert.IsNotNull(indexMethod, "Index method should exist in EmployeeController.");
         }
 
         [Test]
-        public void test_case4()
+        public void TestCreateGetMethodExists()
         {
             // Arrange
-            int nonExistingId = 100; // Assuming there is no employee with ID 100
-
-            // Act
-            var result = _controller.Delete(nonExistingId) as NotFoundResult;
+            MethodInfo createGetMethod = controllerType.GetMethod("Create", new Type[0]);
 
             // Assert
-            Assert.IsNotNull(result);
-            Assert.IsInstanceOf<NotFoundResult>(result);
+            Assert.IsNotNull(createGetMethod, "Create method should exist in EmployeeController.");
         }
 
         [Test]
-        public void test_case5()
+        public void TestCreatePostMethodExists()
         {
             // Arrange
-            int invalidId = -1; // Assuming -1 is an invalid ID
-
-            // Act
-            var result = _controller.Delete(invalidId) as BadRequestResult;
+            MethodInfo createPostMethod = controllerType.GetMethod("Create", new Type[] { typeof(Employee) });
 
             // Assert
-            Assert.IsNotNull(result);
-            Assert.IsInstanceOf<BadRequestResult>(result);
+            Assert.IsNotNull(createPostMethod, "Create POST method should exist in EmployeeController.");
+        }
+
+        [Test]
+        public void TestSearchMethodExists()
+        {
+            // Arrange
+            MethodInfo createPostMethod = controllerType.GetMethod("Search", new Type[] { typeof(string) });
+
+            // Assert
+            Assert.IsNotNull(createPostMethod, "Search method should exist in EmployeeController.");
+        }
+
+
+        [Test]
+        public void TestCreatePostMethodReturnsActionResult()
+        {
+            // Arrange
+            MethodInfo createPostMethod = controllerType.GetMethod("Create", new Type[] { typeof(Employee) });
+            var controller = new EmployeeController();
+
+            // Act
+            var employee = new Employee(); // Create a sample Furniture object
+            var result = createPostMethod.Invoke(controller, new object[] { employee });
+
+            // Assert
+            Assert.IsInstanceOf<IActionResult>(result, "Create (POST) method should return an IActionResult.");
+        }
+
+        [Test]
+        public void TestEmployeeClassExists()
+        {
+            // Arrange
+            Type furnitureType = typeof(dotnetapp.Models.Employee);
+
+            // Assert
+            Assert.IsNotNull(furnitureType, "Employee class should exist.");
+        }
+
+        [Test]
+        public void TestEmployeePropertiesExist()
+        {
+            // Assert
+            Assert.IsNotNull(properties, "Employee class should have properties.");
+            Assert.IsTrue(properties.Length > 0, "Employee class should have at least one property.");
+        }
+
+         [Test]
+        public void TestidProperty()
+        {
+            // Arrange
+            PropertyInfo idProperty = properties.FirstOrDefault(p => p.Name == "id");
+
+            // Assert
+            Assert.IsNotNull(idProperty, "id property should exist.");
+            Assert.AreEqual(typeof(int), idProperty.PropertyType, "id property should have data type 'int'.");
+        }
+
+        [Test]
+        public void TestNameProperty()
+        {
+            // Arrange
+            PropertyInfo productProperty = properties.FirstOrDefault(p => p.Name == "Name");
+
+            // Assert
+            Assert.IsNotNull(productProperty, "Name property should exist.");
+            Assert.AreEqual(typeof(string), productProperty.PropertyType, "Name property should have data type 'string'.");
+        }
+
+        [Test]
+        public void TestDOBProperty()
+        {
+            // Arrange
+            PropertyInfo descriptionProperty = properties.FirstOrDefault(p => p.Name == "DOB");
+
+            // Assert
+            Assert.IsNotNull(descriptionProperty, "DOB property should exist.");
+            Assert.AreEqual(typeof(DateTime), descriptionProperty.PropertyType, "DOB property should have data type 'string'.");
+        }
+
+        [Test]
+        public void TestsalaryProperty()
+        {
+            // Arrange
+            PropertyInfo descriptionProperty = properties.FirstOrDefault(p => p.Name == "salary");
+
+            // Assert
+            Assert.IsNotNull(descriptionProperty, "salary property should exist.");
+            Assert.AreEqual(typeof(decimal), descriptionProperty.PropertyType, "salary property should have data type 'string'.");
+        }
+
+        // [Test]
+        // public void TestDimensionsProperty()
+        // {
+        //     // Arrange
+        //     PropertyInfo descriptionProperty = properties.FirstOrDefault(p => p.Name == "Dimensions");
+
+        //     // Assert
+        //     Assert.IsNotNull(descriptionProperty, "Dimensions property should exist.");
+        //     Assert.AreEqual(typeof(string), descriptionProperty.PropertyType, "Dimensions property should have data type 'string'.");
+        // }
+
+        // [Test]
+        // public void TestPriceProperty()
+        // {
+        //     // Arrange
+        //     PropertyInfo descriptionProperty = properties.FirstOrDefault(p => p.Name == "Price");
+
+        //     // Assert
+        //     Assert.IsNotNull(descriptionProperty, "Price property should exist.");
+        //     Assert.AreEqual(typeof(decimal), descriptionProperty.PropertyType, "Price property should have data type 'string'.");
+        // }
+
+        [Test]
+        public void TestEmployeeDBContext_ClassExists_in_Models()
+        {
+            // Load the assembly at runtime
+            Assembly assembly = Assembly.Load("dotnetapp");
+            Type postType = assembly.GetType("dotnetapp.Models.EmployeeDBContext");
+            Assert.NotNull(postType, "EmployeeDBContext class does not exist.");
+        }
+
+        // [Test]
+        // public void TestEmployee_Folder_Exists()
+        // {
+        //     bool viewsFolderExists = Directory.Exists(@"/home/coder/project/workspace/dotnetapp/Views/Employee");
+
+        //     Assert.IsTrue(viewsFolderExists, "Employee does not exist.");
+        // }
+
+        [Test]
+        public void Test_CreateViewFile_Exists()
+        {
+            string indexPath = Path.Combine(@"/home/coder/project/workspace/dotnetapp/Views/Employee", "Create.cshtml");
+            bool indexViewExists = File.Exists(indexPath);
+
+            Assert.IsTrue(indexViewExists, "Create.cshtml view file does not exist.");
+        }
+
+        [Test]
+        public void Test_IndexViewFile_Exists()
+        {
+            string indexPath = Path.Combine(@"/home/coder/project/workspace/dotnetapp/Views/Employee", "Index.cshtml");
+            bool indexViewExists = File.Exists(indexPath);
+
+            Assert.IsTrue(indexViewExists, "Index.cshtml view file does not exist.");
+        }
+
+        [Test]
+        public void TestSearchViewResult()
+        {
+            // Arrange
+            var controller = new EmployeeController();
+
+            // Act
+            var result = controller.Search("query") as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result, "Search method should return a ViewResult.");
         }
     }
 }
